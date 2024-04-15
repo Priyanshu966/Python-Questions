9e550b2f75660609c0f0464ae0368ca4a025b1e2
Priyanshu <134921189+Priyanshu966@users.noreply.github.com>
2024-04-15 13:37:04 +0530

    Delete 1.conditional_and_loops/1.easy/_1.calculate_simple_interest.py

--- 1.conditional_and_loops/1.easy/_1.calculate_simple_interest.py
+++
@@ -1,6 +0,0 @@
-principal = int(input("Enter the amount borrowed\n"))
-years = float(input("Enter the period in years\n"))
-rate = float(input("Enter rate of interest\n"))
-
-interest = principal * rate * years / 100
-print(f"Simple interest on principal amount ${principal} for a period of {years} years at the rate of {rate}% is ${interest}")
