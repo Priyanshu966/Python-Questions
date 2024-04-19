9cd317c79f2b9ee558903e696d0f541c45f089da
Priyanshu <134921189+Priyanshu966@users.noreply.github.com>
2024-04-19 08:05:35 +0530

    Add files via upload

--- 2.patterns/1.easy/5.Diamond/diamond.py
+++ 2.patterns/1.easy/5.Diamond/diamond.py
@@ -2,4 +2,51 @@
 #          ***
 #         *****
 #          ***
-#           *
+#           *        here n = 5
+
+
+n = int(input("Enter a odd No\n"))
+
+half = n // 2 + 1
+
+## For first half
+i = 1
+
+while i <= half:
+    j = 1
+    k = 1
+    l = 1
+    while j <= half - i:
+        print(' ',end='')
+        j += 1
+    while k <= i:
+        print('*',end='')
+        k += 1
+    while l <= i - 1:
+        print('*',end='')
+        l += 1
+    i += 1
+    print('')
+
+## For second half
+
+m = 1
+
+while m <= half - 1:
+    j = 1
+    k = 1
+    l = 1
+    while j <= m:
+        print(' ',end='')
+        j += 1
+    while k <= half - m:
+        print('*',end='')
+        k += 1
+    while l <= half - 1 - m :
+        print('*',end='')
+        l += 1
+    m += 1
+    print('')
+
+
+
