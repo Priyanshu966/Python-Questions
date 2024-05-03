d12923ba3a863534674856eff8a157fa8791200e
Priyanshu <134921189+Priyanshu966@users.noreply.github.com>
2024-05-03 11:46:23 +0530

    Add files via upload

--- 2.patterns/1.easy/6.Sum/sum.py
+++ 2.patterns/1.easy/6.Sum/sum.py
@@ -3,3 +3,16 @@
 #         1+2+3=6
 #         1+2+3+4=10
 #         1+2+3+4+5=15  here n = 5
+
+n = int(input('Please Enter a Number\n'))
+
+for i in range(1,n + 1):
+    sum = 0
+    for j in range(1,i + 1):
+        sum += j
+        print(j,end='')
+        if j != i:
+            print('+',end='')
+        else:
+            print(f'={j}')
+
