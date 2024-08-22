e8f01656b797b57aa277c87c13045160fa207f0e
Priyanshu <134921189+Priyanshu966@users.noreply.github.com>
2024-08-22 16:40:54 +0530

    Add files via upload

--- 6.recursion/1.easy/27.Print-Reverse-Of-String/print_reverse_of_string.py
+++ 6.recursion/1.easy/27.Print-Reverse-Of-String/print_reverse_of_string.py
@@ -1,1 +1,25 @@
-def get_reverse()
+def get_reverse_main(string,i):
+    if i < 0:
+        return ""
+
+    output = get_reverse_main(string, i - 1)
+
+    return string[i] + output
+
+
+
+
+def get_reverse(string):
+    if len(string) == 0:
+        raise ValueError()
+    return get_reverse_main(string, len(string) - 1)
+
+
+while True:
+    try:
+        string = input("Enter a string to reverse\n")
+        answer = get_reverse(string)
+        print(answer)
+        break
+    except Exception:
+        print("Wrong Input, Try Again\n")
